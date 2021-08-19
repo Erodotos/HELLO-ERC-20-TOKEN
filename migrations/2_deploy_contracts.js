54d22eb8c9492518ad093ff903f89673d573d0a3
Erodotos <edemet01@cs.ucy.ac.cy>
2021-08-19 17:39:35 +0300

    Revert "First ommit"
    This reverts commit 355de56c04ecbc7ef6194b1d9def564dafa974b9.

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config({ path: '../.env' });
-const HelloToken = artifacts.require("./HelloToken.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(HelloToken, process.env.TOTAL_SUPPLY);
-};
